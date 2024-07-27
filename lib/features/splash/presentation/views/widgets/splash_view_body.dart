25f078740137fbde55d120fd82c5d940d121ba28
mohamedali392001 <ly8197884@gmail.com>
2024-07-26 19:32:08 -0700

    edit in slide animation part 2

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -49,7 +49,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initSlidingAnimation() {
     animationController = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 2),
+      duration: const Duration(seconds: 1),
     );
     slidingAnimation =
         Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
@@ -61,7 +61,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
   }
 
   void navigateToHomeView() {
-    Future.delayed(const Duration(seconds: 4), () {
+    Future.delayed(const Duration(seconds: 3), () {
       Get.to(() => const HomeView(),
           transition: Transition.fade, duration: kDurationTransation);
     });
