df72be0ce0a43a2963d26a037b67435149c07833
mohamedali392001 <ly8197884@gmail.com>
2024-07-27 21:15:43 -0700

    create custom scroll view

---
+++ lib/features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_item.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+        delegate: SliverChildBuilderDelegate(childCount: 10, (context, index) {
+      return const Padding(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: BestSellerItem(),
+      );
+    }));
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,8 @@
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/bookly_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -11,20 +12,29 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          BooklyListView(),
-          SizedBox(height: 50,),
-          Text('Best Seller', style: Styles.textStyle18,),
-          SizedBox(
-            height: 20,
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: CustomScrollView(physics: BouncingScrollPhysics(), slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(),
+              BooklyListView(),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Best Seller',
+                style: Styles.textStyle18,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
           ),
-          BestSellerItem(),
-        ],
-      ),
+        ),
+        BestSellerListView(),
+      ]),
     );
   }
 }
