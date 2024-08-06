2399dbb2064d0429dd047b75908c794e44ad29bc
mohamedali392001 <ly8197884@gmail.com>
2024-08-06 00:55:41 -0700

    edit

--- lib/features/home/presentation/manager/search_cubit/search_cubit.dart
+++ lib/features/home/presentation/manager/search_cubit/search_cubit.dart
@@ -7,19 +7,17 @@ part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
   SearchCubit() : super(SearchInitial());
-  
 
   void onSearch(context, String value) {
-    if(value.isEmpty) {
+    if (value.isEmpty) {
       emit(SearchInitial());
-    }
-    else {
+    } else {
       List<BookModel> data = BlocProvider.of<NewestBooksCubit>(context)
-        .bookList
-        .where((element) =>
-            element.volumeInfo.title!.toLowerCase().contains(value))
-        .toList();
-        emit(SearchSuccess(books: data));
+          .bookList
+          .where((element) =>
+              element.volumeInfo.title!.toLowerCase().contains(value))
+          .toList();
+      emit(SearchSuccess(books: data));
     }
   }
 }
